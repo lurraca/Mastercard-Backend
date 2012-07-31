63b087f2e5c0dd3c4aeaa4dd05984774af85dd1b
Ubuntu <ubuntu@ip-10-244-250-205.ec2.internal>
2012-07-31 01:02:29 +0000

    Adding repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mastercard::Application.config.secret_token = '2d3d3545401d04356dc2995d0bef8d9024c1bfd306483dfcaa1c4139dd7838a0391f29d301bdbd727043aa78018fe4d6817352452e011f0fd6aa465b267d6558'
